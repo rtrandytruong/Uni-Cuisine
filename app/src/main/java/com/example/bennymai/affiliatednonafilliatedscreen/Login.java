06057cfee486655fd28293037eddc66b76a3a43d
gdotzheng <37161400+gdotzheng@users.noreply.github.com>
2018-04-25 23:50:30 -0400

    Firebase integration

--- app/src/main/java/com/example/bennymai/affiliatednonafilliatedscreen/Login.java
+++ app/src/main/java/com/example/bennymai/affiliatednonafilliatedscreen/Login.java
@@ -5,10 +5,47 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class Login extends AppCompatActivity implements View.OnClickListener{
 
-    private Button login;
+    TextView Signup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Signup = (TextView) findViewById(R.id.textViewSignup);
+
+        findViewById(R.id.textViewSignup).setOnClickListener(this);
+    }
+
+    /*@Override
+    public void onClick(View view) {
+
+        switch(view.getId()){
+            case R.id.textViewSignup:
+
+                startActivity(new Intent(this, Signup.class));
+
+                break;
+
+        }*/
+
+    @Override
+    public void onClick(View v) {
+        if (v==Signup) {
+            startActivity(new Intent(this, Signup.class));
+        }
+
+    }
+}
+
+
+    /*private Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,3 +64,4 @@ public class Login extends AppCompatActivity implements View.OnClickListener{
         }
     }
 }
+*/
--- app/src/main/java/com/example/bennymai/affiliatednonafilliatedscreen/Signup.java
+++ app/src/main/java/com/example/bennymai/affiliatednonafilliatedscreen/Signup.java
@@ -1,14 +1,129 @@
 package com.example.bennymai.affiliatednonafilliatedscreen;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Signup extends AppCompatActivity implements View.OnClickListener{
 
-    private Button signup;
+    ProgressBar progressBar;
+    EditText editTextEmail, editTextPassword;
+    TextView Login;
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        progressBar = (ProgressBar) findViewById(R.id.progressbar);
+
+        Login = (TextView) findViewById(R.id.textViewLogin);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        findViewById(R.id.buttonSignUp).setOnClickListener(this);
+        findViewById(R.id.textViewLogin).setOnClickListener(this);
+    }
+
+    private void registerUser() {
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        if (email.isEmpty()) {
+            editTextEmail.setError("Email is required");
+            editTextEmail.requestFocus();
+            return;
+
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            editTextEmail.setError("Please enter a valid Email");
+            editTextEmail.requestFocus();
+            return;
+
+        }
+
+
+
+        if (password.isEmpty()) {
+            editTextPassword.setError("Password is required");
+            editTextPassword.requestFocus();
+            return;
+
+
+        }
+
+        if (password.length()<6) {
+            editTextPassword.setError("Minimum of password should be 6");
+            editTextPassword.requestFocus();
+            return;
+
+        }
+
+        progressBar.setVisibility(View.VISIBLE);
+
+        mAuth.createUserWithEmailAndPassword(email, password) .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                progressBar.setVisibility(View.GONE);
+                if (task.isSuccessful()) {
+                    Toast.makeText(getApplicationContext(), "User Registered Successful", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(getApplicationContext(),"Some error occured", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.buttonSignUp:
+                registerUser();
+                break;
+        }
+        if (v==Login) {
+            startActivity(new Intent(this, Login.class));
+        }
+
+    }
+
+}
+
+    /*@Override
+    public void onClick(View view) {
+        switch(view.getId()) {
+            case R.id.buttonSignUp:
+                registerUser();
+                break;
+
+            case R.id.textViewLogin:
+                startActivity(new Intent(this, MainActivity.class));
+                break;
+        }
+    }
+}*/
+
+    /*private Button signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,3 +142,4 @@ public class Signup extends AppCompatActivity implements View.OnClickListener{
         }
     }
 }
+*/
